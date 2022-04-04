8fd36545ac62562b1d39be414fefbec476462c18
DK09876 <55561168+DK09876@users.noreply.github.com>
2022-04-04 10:45:43 -0500

    First Commit
    Initializing flutter app, tests connection with back4app (backend)

---
+++ tap_in/android/app/src/main/kotlin/com/example/tap_in/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tap_in
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tap_in/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:tap_in/constants.dart';
+
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = kParseApplicationId;
+  final keyClientKey = kParseClientKey;
+  final keyParseServerUrl = kUrl;
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
+  var firstObject = ParseObject('FirstClass')
+    ..set(
+        'message', 'Hey ! First message from Flutter. Parse is now connected');
+  await firstObject.save();
+
+  print('done');
+}
