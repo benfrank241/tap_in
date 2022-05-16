cb407f51f4a0778401069a003db5652ce8aed3ce
DK09876 <55561168+DK09876@users.noreply.github.com>
2022-05-16 18:59:49 -0500

    Meh

--- tapin/lib/screens/ImageUpload/ImageUpload.dart
+++ tapin/lib/screens/ImageUpload/ImageUpload.dart
@@ -1,57 +1,57 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
+// import 'package:file_picker/file_picker.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:path/path.dart';
 
-class Imageuploader extends StatefulWidget {
-  @override
-  _ImageUploader createState() => _ImageUploader();
-}
+// class Imageuploader extends StatefulWidget {
+//   @override
+//   _ImageUploader createState() => _ImageUploader();
+// }
 
-class _ImageUploader extends State<Imageuploader> {
-  PlatformFile? pickedFile;
+// class _ImageUploader extends State<Imageuploader> {
+//   PlatformFile? pickedFile;
 
-  Future SelectFile() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.image,
-    );
+//   Future SelectFile() async {
+//     final result = await FilePicker.platform.pickFiles(
+//       type: FileType.image,
+//     );
 
-    if (result == null) return;
+//     if (result == null) return;
 
-    setState(() {
-      pickedFile = result.files.first;
-    });
-  }
+//     setState(() {
+//       pickedFile = result.files.first;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (pickedFile != null)
-            Expanded(
-              child: Container(
-                child: Center(
-                    child: Image.file(
-                  File(pickedFile!.path!),
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                )),
-              ),
-            ),
-          ElevatedButton(onPressed: () {}, child: Text('Upload')),
-          ElevatedButton(
-              onPressed: () {
-                SelectFile();
-              },
-              child: Text('Select')),
-        ],
-      ),
-    ));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Center(
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           if (pickedFile != null)
+//             Expanded(
+//               child: Container(
+//                 child: Center(
+//                     child: Image.file(
+//                   File(pickedFile!.path!),
+//                   width: double.infinity,
+//                   fit: BoxFit.cover,
+//                 )),
+//               ),
+//             ),
+//           ElevatedButton(onPressed: () {}, child: Text('Upload')),
+//           ElevatedButton(
+//               onPressed: () {
+//                 SelectFile();
+//               },
+//               child: Text('Select')),
+//         ],
+//       ),
+//     ));
+//   }
+// }
